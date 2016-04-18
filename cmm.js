5e1c43b7db25cdfb36ab4956587c838e14c32380
Ricard Gascons <gascons1995@gmail.com>
2016-04-18 09:31:50 +0200

    Add simple command line support

--- cmm.js
+++ cmm.js
@@ -1,5 +1,6 @@
 var fs = require("fs");
 var jison = require("jison");
+var readline = require('readline');
 
 var bnf = fs.readFileSync("parser/grammar.jison", "utf8");
 var parser = new jison.Parser(bnf);
@@ -8,5 +9,14 @@ function exec(input) {
     return parser.parse(input);
 }
 
-var prog = exec("4 * 5");
-console.log(prog);
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+process.stdout.write(">>> ");
+rl.on('line', function(line){
+    var instr = exec(line);
+    console.log(instr);
+    process.stdout.write(">>> ");
+});
