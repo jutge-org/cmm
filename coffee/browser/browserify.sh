3bcfcb53b5655db7ccefd8f45973253ee9ab95e2
pau oliver <pau.oliver@est.fib.upc.edu>
2016-05-29 18:34:33 +0200

    Restructure files: coffee folder is now the project root

--- coffee/browser/browserify.sh
+++
@@ -1,5 +0,0 @@
-# The -t option is used to specify plugins. In this case coffeeify allows
-# to require .coffee files and jisonify allows to require .jison files
-browserify -t coffeeify -t jisonify --extension=".coffee" js/main.coffee -o js/main.js
-
-uglifyjs < js/main.js > js/main.min.js
