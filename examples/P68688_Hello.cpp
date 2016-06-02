734176b8b1979b96fb31090912eb81772be309db
Albert Segarra Roca <albertsgrc@gmail.com>
2016-06-02 23:08:45 +0200

    Remove examples folder

--- examples/P68688_Hello.cpp
+++
@@ -1,9 +0,0 @@
-//Prints out the message "Hello world!", no matter what the input is.
-
-#include <iostream>
-using namespace std;
-
-int main(){
-	cout << "Hello world!" << endl;
-}
-
--- examples/funcall.cpp
+++
@@ -1,7 +0,0 @@
-int sum_one(int i) {
-    return i+1;
-}
-
-int main() {
-    cout << sum_one(3.14) << endl;
-}
