d1f43455f732e3e6a0f3ccaf97aa8af22bee88f6
Ricard Gascons <gascons1995@gmail.com>
2016-09-08 16:00:32 +0200

    Remove browser directory
    This is part of a series of commits for the decoupling of
    the compiler from its frontend with the aim of making it more maintainable.

--- browser/browserify.sh
+++
@@ -1,6 +0,0 @@
-# The -t option is used to specify plugins. In this case coffeeify allows
-# to require .coffee files and jisonify allows to require .jison files
-coffee -c js/main.coffee
-browserify -t coffeeify -t jisonify --extension=".coffee" js/run.coffee -o js/run.js
-sed -i -e 's/onmessage,//g' js/run.js
-uglifyjs < js/run.js > js/run.min.js
