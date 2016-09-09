db5b9bc6c5469d2df5d8d8224a574e50afeac7a7
Ricard Gascons <gascons1995@gmail.com>
2016-09-09 02:43:39 +0200

    Add gulpfile
    This is part of a series of commits for the decoupling of
    the compiler from its frontend with the aim of making it more maintainable.

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+    browserify = require('browserify'),
+    coffeeify = require('coffeeify'),
+    jisonify = require('jisonify'),
+    source = require('vinyl-source-stream'),
+    uglify = require('gulp-uglify'),
+    buffer = require('vinyl-buffer');
+
+
+gulp.task('build', function() {
+    return browserify('./index.coffee', {extensions:['.coffee']})
+        .transform(coffeeify)
+        .transform(jisonify)
+        .bundle()
+        .pipe(source('index.min.js')) // gives streaming vinyl file object
+        .pipe(buffer()) // convert from streaming to buffered vinyl file object
+        .pipe(uglify()) // now gulp-uglify works
+        .pipe(gulp.dest('./lib'))
+});
+
+gulp.task('default', ['build'], function() {
+});
