9d52d5ba1383b11d5100345010e7f5e5977e79fd
Albert Segarra Roca <albertsgrc@gmail.com>
2017-04-26 16:35:13 +0200

    Add temporaries test

---
+++ test/programs/runtime/temporaries/program.cc
@@ -0,0 +1,25 @@
+/*
+
+description(Test that temporaries are properly scoped and not get overwritten on function calls)
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int rec(int x) {
+    if (x <= 0) return 1;
+    int r = ((2 % 3)*(rec(x/100) < x))*((1/1) + (6+rec(x/100)) + ((5*3) - (1*rec(x - 1))));
+    cout << x << ' ' << r << endl;
+    return r;
+}
+
+int f(int x) {
+    return ((5.8 + 3)/(2*3)) - ((5+2)*(3.5/3)) * ((5.8 + 3)/(2*3)) - ((5+2)*(rec(x)/3.5));
+}
+
+
+int main() {
+    int x;
+    while (cin >> x) cout << f(x) << endl;
+}
